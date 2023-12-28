c67ad704c5b018ce3f74a6b032793a40471bb15b
pritamsap <pritamsap2@gmail.com>
2023-12-28 13:01:52 -0500

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by pritam on 2023-12-28.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
